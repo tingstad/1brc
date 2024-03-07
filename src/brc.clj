44bb7401dfcb15f6e74833c0724ec93dcfbc6278
Richard Tingstad <richard.tingstad@amedia.no>
2024-03-07 10:39:00 +0100

    cleanup

--- src/brc.clj
+++ src/brc.clj
@@ -13,40 +13,20 @@
 (defn length [line]
   (count (.getBytes line "UTF-8")))
 
-(comment
-  (defn summer [w1 w2] [(+ (nth w1 0) (nth w2 0))
-                        (+ (nth w1 1) (nth w2 1))
-                        (+ (nth w1 2) (nth w2 2))])
-  (defn wc [lines]
-    (let [countline
-          (fn [line]
-            [1, (words line), (count line)])]
-      (reduce summer
-              [0 0 0]
-              (map countline lines))))
-  )
-
 (defn summer [w1 w2]
-  (let [sum (mapv + w1 w2)]
-    sum
-    ))
-
-(defn summer3 [w1 w2] [(+ (nth w1 0) (nth w2 0))
-                      (+ (nth w1 1) (nth w2 1))
-                      (+ (nth w1 2) (nth w2 2))])
-(def summer2 (partial mapv +))
+  (mapv + w1 w2))
 
 (defn wc [lines]
   (->> lines
-       (map (fn [line] [1 (words line) (+ 1 (length line))]) ,,,)
-       (reduce summer  ,,,))
+       (map (fn [line] [1 (words line) (+ 1 (length line))]),,,)
+       (reduce summer,,,))
   )
 
 ;;(defn main [_opts] (prn (wc (readlines file 10))))
 
 (comment
-(prn (wc (readlines file 100000)))
-)
+  (prn (wc (readlines file 100000)))
+  )
 
 (defn echo [result]
   (format "%d %d %d" (get result 0) (get result 1) (get result 2)))
@@ -59,9 +39,3 @@
 (comment
   (wc (readlines file 5))
   )
-
-(summer [1 2 3] [2 5 8])
-(map + [1 2 3] [2 5 8])
-
-(summer2 [1 2 3] [2 5 8])
-(summer [1 2 3] [2 5 8])
